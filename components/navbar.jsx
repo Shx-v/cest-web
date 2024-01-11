83878a18e4d38c53b2b5931cd346018e62a551fb
shiv <shivranjanbharadwaj@gmail.com>
2024-01-11 18:58:58 +0530

    navPadding

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     const navOptions = ["Home", "Team", "Gallery", "About"];
 
     return ( 
-        <nav className='flex flex-row items-center justify-between px-[2%] py-[1%] bg-black bg-opacity-50 fixed w-[100vw]'>
+        <nav className='flex flex-row items-center justify-between px-[20px] py-[10px] bg-black bg-opacity-50 fixed w-[100vw]'>
             <img src="./CEST_Logo.svg" alt="CEST Club" className='select-none w-[4vw]'/>
             <div className='flex flex-row gap-[1vw] select-none'>
                 {navOptions.map((option, index) => (
